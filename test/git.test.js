407271afc883819c427437f19aac5b9a81251134
lordsiinbal <nutsudragneel50@gmail.com>
2022-10-01 13:03:46 +0800

    New test Added

--- test/git.test.js
+++ test/git.test.js
@@ -64,3 +64,8 @@ describe("Testing add()", function(){
         expect(output_status).to.equal('You have 1 change/s.\n.github/workflows/actions.yml');
     });
 })
+describe("Testing add()", function(){
+    let func=new Function
+    let res=func.new()
+    expect(res).to.equal('Successfully');
+})
