b25760ed33a2331ed82aa4a4141501839eba7e13
lordsiinbal <79137266+lordsiinbal@users.noreply.github.com>
2022-10-01 21:35:21 +0800

    Update git-command.js

--- models/git-command.js
+++ models/git-command.js
@@ -9,7 +9,7 @@ class GitCommand {
         return "Initialized as empty Git repository.";
     }
     test_sample(){
-        return true
+        return false
     }
     //Command: git status
     // status(){}
